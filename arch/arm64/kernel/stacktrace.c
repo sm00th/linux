6017ecbabbbf44091b0b0f65be3f8da2f6f70a06
Madhavan T. Venkataraman <madvenka@linux.microsoft.com>
2022-01-17 08:56:06 -0600

    arm64: Create a list of SYM_CODE functions, check return PC against list
    SYM_CODE functions don't follow the usual calling conventions. Check if the
    return PC in a stack frame falls in any of these. If it does, consider the
    stack trace unreliable.
    
    Define a special section for unreliable functions
    =================================================
    
    Define a SYM_CODE_END() macro for arm64 that adds the function address
    range to a new section called "sym_code_functions".
    
    Linker file
    ===========
    
    Include the "sym_code_functions" section under read-only data in
    vmlinux.lds.S.
    
    Initialization
    ==============
    
    Define an early_initcall() to create a sym_code_functions[] array from
    the linker data.
    
    Unwinder check
    ==============
    
    Add a reliability check in unwind_check_reliability() that compares a
    return PC with sym_code_functions[]. If there is a match, then return
    failure.
    
    Signed-off-by: Madhavan T. Venkataraman <madvenka@linux.microsoft.com>
    Reviewed-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/kernel/stacktrace.c
+++ arch/arm64/kernel/stacktrace.c
@@ -18,11 +18,40 @@
 #include <asm/stack_pointer.h>
 #include <asm/stacktrace.h>
 
+struct code_range {
+	unsigned long	start;
+	unsigned long	end;
+};
+
+static struct code_range	*sym_code_functions;
+static int			num_sym_code_functions;
+
+int __init init_sym_code_functions(void)
+{
+	size_t size = (unsigned long)__sym_code_functions_end -
+		      (unsigned long)__sym_code_functions_start;
+
+	sym_code_functions = (struct code_range *)__sym_code_functions_start;
+	/*
+	 * Order it so that sym_code_functions is not visible before
+	 * num_sym_code_functions.
+	 */
+	smp_mb();
+	num_sym_code_functions = size / sizeof(struct code_range);
+
+	return 0;
+}
+early_initcall(init_sym_code_functions);
+
 /*
  * Check the stack frame for conditions that make further unwinding unreliable.
  */
 static void unwind_check_reliability(struct unwind_state *state)
 {
+	const struct code_range *range;
+	unsigned long pc;
+	int i;
+
 	if (state->fp == state->final_fp) {
 		/* Final frame; no more unwind, no need to check reliability */
 		return;
@@ -35,6 +64,32 @@ static void unwind_check_reliability(struct unwind_state *state)
 	 */
 	if (!__kernel_text_address(state->pc))
 		state->reliable = false;
+
+	/*
+	 * Check the return PC against sym_code_functions[]. If there is a
+	 * match, then the consider the stack frame unreliable.
+	 *
+	 * As SYM_CODE functions don't follow the usual calling conventions,
+	 * we assume by default that any SYM_CODE function cannot be unwound
+	 * reliably.
+	 *
+	 * Note that this includes:
+	 *
+	 * - Exception handlers and entry assembly
+	 * - Trampoline assembly (e.g., ftrace, kprobes)
+	 * - Hypervisor-related assembly
+	 * - Hibernation-related assembly
+	 * - CPU start-stop, suspend-resume assembly
+	 * - Kernel relocation assembly
+	 */
+	pc = state->pc;
+	for (i = 0; i < num_sym_code_functions; i++) {
+		range = &sym_code_functions[i];
+		if (pc >= range->start && pc < range->end) {
+			state->reliable = false;
+			return;
+		}
+	}
 }
 
 /*
